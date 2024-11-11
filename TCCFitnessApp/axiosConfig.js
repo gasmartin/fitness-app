081755cf5ee72fee502e4a4880d4fd84f6163e5d
Gabriel San Martin <gabrielsanmartin1275@gmail.com>
2024-11-10 23:09:45 -0400

    Configurando fluxo de refresh do token com o axios...

--- TCCFitnessApp/axiosConfig.js
+++ TCCFitnessApp/axiosConfig.js
@@ -1,23 +1,88 @@
 import axios from 'axios';
-
-import { getToken } from './helpers/token';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { CommonActions, useNavigation } from '@react-navigation/native';
 
 const api = axios.create({
   baseURL: 'http://192.168.25.42:8000',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
-api.interceptors.request.use(
-  async (config) => {
-    const token = await getToken();
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+let isRefreshing = false;
+let refreshSubscribers = [];
+
+const onRefreshed = (newToken) => {
+  refreshSubscribers.map(callback => callback(newToken));
+  refreshSubscribers = [];
+};
+
+const addRefreshSubscriber = (callback) => {
+  refreshSubscribers.push(callback);
+};
+
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem('accessToken');
+
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
+
+  return config;
+}, (error) => {
+  return Promise.reject(error)
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+
+        try {
+          const refreshToken = await AsyncStorage.getItem('refreshToken');
+          const response = await axios.post('http://192.168.25.42:8000/users/refresh-token', { refreshToken });
+        
+          const newAccessToken = response.data.accessToken;
+          await AsyncStorage.setItem('accessToken', newAccessToken);
+
+          isRefreshing = false;
+          onRefreshed(newAccessToken);
+
+          originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+          return api(originalRequest);
+        }
+        catch (err) {
+          isRefreshing = false;
+
+          await AsyncStorage.removeItem('accessToken');
+          await AsyncStorage.removeItem('refreshToken');
+
+          const navigation = useNavigation();
+          navigation.dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [{ name: "Login" }],
+            })
+          );
+
+          return Promise.reject(err);
+        }
+      }
+
+      return new Promise((resolve) => {
+        addRefreshSubscriber((newToken) => {
+          originalRequest.headers['Authorization'] = 'Bearer ' + newToken;
+          resolve(axios(originalRequest));
+        });
+      });
     }
-    return config;
-  },
-  (error) => {
+
     return Promise.reject(error);
   }
 );
